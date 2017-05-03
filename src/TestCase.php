8504ee02135f704f454f082d03a896766e4b8845
Sergey Makinen <sergey@makinen.ru>
2017-05-03 20:09:55 +0300

    Reorganize TestCase initialization to improve IDE auto completion

---
+++ src/TestCase.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Common tests stuff
+ *
+ * @see       https://github.com/sergeymakinen/tests
+ * @copyright Copyright (c) 2017 Sergey Makinen (https://makinen.ru)
+ * @license   https://github.com/sergeymakinen/tests/blob/master/LICENSE MIT License
+ */
+
+namespace SergeyMakinen\Tests;
+
+/** @var \PHPUnit_Runner_Version|\PHPUnit\Runner\Version $class */
+$class = class_exists('PHPUnit_Runner_Version') ? 'PHPUnit_Runner_Version' : 'PHPUnit\Runner\Version';
+$version = $class::id();
+if (version_compare($version, '6.0', '>=')) {
+    class_alias('SergeyMakinen\Tests\TestCaseVersion6', 'SergeyMakinen\Tests\DynamicTestCase');
+} elseif (version_compare($version, '5.0', '>=')) {
+    class_alias('SergeyMakinen\Tests\TestCaseVersion5', 'SergeyMakinen\Tests\DynamicTestCase');
+} else {
+    class_alias('SergeyMakinen\Tests\TestCaseVersion4', 'SergeyMakinen\Tests\DynamicTestCase');
+}
+
+/**
+ * Version independent PHPUnit test case.
+ * @mixin \PHPUnit_Framework_TestCase
+ * @mixin \PHPUnit\Framework\TestCase
+ */
+class TestCase extends DynamicTestCase
+{
+}
